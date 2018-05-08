7b8c78afa5d0355966533c68dece64072efa995a
Arafat <arafat@iitkgp.ac.in>
2018-05-08 17:19:10 +0530

    Adding RBT

--- example_tester.go
+++ example_tester.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 
-	list "github.com/Arafatk/dataviz/lists/doublylinkedlist"
+	list "github.com/Arafatk/dataviz/maps/treemap"
 )
 
 func main() {
-	list := list.New()
-	list.Add("a")
-	list.Add("b", "c")
-	fmt.Println(list.Visualizer("out.png"))
+	m := list.NewWithIntComparator()
+	m.Put(5, "e")
+	m.Put(6, "f")
+	m.Put(7, "g")
+	m.Put(3, "c")
+	m.Put(4, "d")
+	m.Put(1, "x")
+	m.Put(2, "b")
+	m.Put(1, "a") //overwrite
+	fmt.Println(m.Visualizer("out.png"))
 }
---
+++ trees/redblacktree/serialization.go
@@ -0,0 +1,39 @@
+
+
+
+
+package redblacktree
+
+import (
+	"encoding/json"
+	"github.com/Arafatk/dataviz/containers"
+	"github.com/Arafatk/dataviz/utils"
+)
+
+func assertSerializationImplementation() {
+	var _ containers.JSONSerializer = (*Tree)(nil)
+	var _ containers.JSONDeserializer = (*Tree)(nil)
+}
+
+// ToJSON outputs the JSON representation of list's elements.
+func (tree *Tree) ToJSON() ([]byte, error) {
+	elements := make(map[string]interface{})
+	it := tree.Iterator()
+	for it.Next() {
+		elements[utils.ToString(it.Key())] = it.Value()
+	}
+	return json.Marshal(&elements)
+}
+
+// FromJSON populates list's elements from the input JSON representation.
+func (tree *Tree) FromJSON(data []byte) error {
+	elements := make(map[string]interface{})
+	err := json.Unmarshal(data, &elements)
+	if err == nil {
+		tree.Clear()
+		for key, value := range elements {
+			tree.Put(key, value)
+		}
+	}
+	return err
+}
