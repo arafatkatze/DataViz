de385375bf22bf8b1b468acffb173b3b0b6520ab
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-21 21:11:26 +0800

    Finally added for all datastructure
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- viz/wrapper.go
+++ viz/wrapper.go
@@ -5,7 +5,15 @@ import (
 	"log"
 	"reflect"
 
+	"github.com/pennz/DataViz/lists/arraylist"
+	"github.com/pennz/DataViz/lists/doublylinkedlist"
+	"github.com/pennz/DataViz/lists/singlylinkedlist"
+	"github.com/pennz/DataViz/maps/treemap"
+	"github.com/pennz/DataViz/stacks/arraystack"
+	"github.com/pennz/DataViz/trees/avltree"
 	"github.com/pennz/DataViz/trees/binaryheap"
+	"github.com/pennz/DataViz/trees/btree"
+	"github.com/pennz/DataViz/trees/redblacktree"
 )
 
 type Wrapper interface {
@@ -33,6 +41,37 @@ func hookTable() map[reflect.Type]([]string) {
 	toHook := make(map[reflect.Type]([]string))
 	bhp := binaryheap.NewWithIntComparator()
 	toHook[reflect.TypeOf(*bhp)] = []string{"Push", "Pop"} // not possible to https://stackoverflow.com/questions/51800637/struct-type-as-map-key
+
+	arraylist := arraylist.New()
+	doublylinkedlist := doublylinkedlist.New()
+	singlylinkedlist := singlylinkedlist.New()
+	// treemap := treemap.NewWith(comparator utils.Comparator)
+	treemap := treemap.NewWithIntComparator()
+	// treemap := treemap.NewWithStringComparator()
+	arraystack := arraystack.New()
+	// avltree := avltree.NewWith(comparator utils.Comparator)
+	avltree := avltree.NewWithIntComparator()
+	// avltree := avltree.NewWithStringComparator()
+	// binaryheap := binaryheap.NewWith(comparator utils.Comparator)
+	binaryheap := binaryheap.NewWithIntComparator()
+	// binaryheap := binaryheap.NewWithStringComparator()
+	// btree := btree.NewWith(order int, comparator utils.Comparator)
+	// btree := btree.NewWithIntComparator(order int)
+	btree := btree.NewWithIntComparator(1)
+	// btree := btree.NewWithStringComparator(order int)
+	// redblacktree := redblacktree.NewWith(comparator utils.Comparator)
+	redblacktree := redblacktree.NewWithIntComparator()
+	// redblacktree := redblacktree.NewWithStringComparator()
+	toHook[reflect.TypeOf(*arraylist)] = []string{"Add", "Remove", "Clear", "Swap", "Insert"}
+	toHook[reflect.TypeOf(*doublylinkedlist)] = []string{"Add", "Remove", "Clear", "Swap", "Insert"}
+	toHook[reflect.TypeOf(*singlylinkedlist)] = []string{"Add", "Remove", "Clear", "Swap", "Insert"}
+	toHook[reflect.TypeOf(*treemap)] = []string{"Put", "Remove", "Clear"}
+	toHook[reflect.TypeOf(*arraystack)] = []string{"Push", "Pop", "Clear"}
+	toHook[reflect.TypeOf(*avltree)] = []string{"Push", "Pop", "Remove", "Clear"}
+	toHook[reflect.TypeOf(*binaryheap)] = []string{"Push", "Pop", "Clear"}
+	toHook[reflect.TypeOf(*btree)] = []string{"Put", "Remove", "Clear"}
+	toHook[reflect.TypeOf(*redblacktree)] = []string{"Put", "Remove", "Clear"}
+
 	return toHook
 }
 
