c9e40d9e51718b336149ce8d2784a8b79c89a6fa
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-24 15:32:18 +0800

    Good Game
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- main.go
+++ main.go
@@ -44,12 +44,23 @@ func readCloser2String(rc io.ReadCloser) string {
 	return newStr
 }
 
+func read2buf(rc io.ReadCloser) *bytes.Buffer {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(rc)
+	return buf
+}
+
 func compileHandler_debug(c *gin.Context) {
 	log.Printf("in deubug%v\n", readCloser2String(c.Request.Body))
 }
 
 func compileHandler(c *gin.Context) {
 	//log.Printf("%v\n", readCloser2String(c.Request.Body))
-	response, _ := http.Post("https://go-algorithm-dev-mqqjokeeppul8.herokuapp.com/compile_debug", "application/x-www-form-urlencoded; charset=UTF-8", c.Request.Body)
+	// https://github.com/gin-gonic/gin#try-to-bind-body-into-different-structs
+	// The normal methods for binding request body consumes c.Request.Body and
+	// they cannot be called multiple times.
+	buf := read2buf(c.Request.Body)
+	var relay io.Reader = bytes.NewReader(buf.Bytes())
+	response, _ := http.Post("https://go-algorithm-dev-mqqjokeeppul8.herokuapp.com/compile_debug", "application/x-www-form-urlencoded; charset=UTF-8", relay)
 	log.Println(readCloser2String(response.Body))
 }
